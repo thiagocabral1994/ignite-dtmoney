c4791f7eb53ff73b5945e218b83af1026dc7422e
Thiago Cabral <thiago.cabral@ice.ufjf.br>
2021-03-27 13:39:36 -0300

    feat: estilização do summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Dashboard } from "./components/Dashboard";
 
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
@@ -7,7 +8,7 @@ export function App() {
   return (
     <>
       <Header />
-      <h1>Hello World!</h1>
+      <Dashboard />
       <GlobalStyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: var(--backgroud);
+        background: var(--background);
         -webkit-font-smoothing: antialiased;
     }
 
