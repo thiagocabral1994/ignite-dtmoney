6e3d49789e0369c7fea3de35be9ca25de4453fa3
Thiago Cabral <thiago.cabral@ice.ufjf.br>
2021-03-27 13:16:00 -0300

    feat: header da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { GlobalStyle } from "./styles/global";
 import React from "react";
+
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <h1>Hello World!</h1>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
